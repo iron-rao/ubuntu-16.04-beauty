1b1c64bc97094ae2a575f5e0f14f79a632919c3c
zhuyy <zhuyy@shanghaitech.edu.cn>
2018-11-11 12:41:11 +0800

    2018.11.11

--- new.sh
+++ new.sh
@@ -4,6 +4,18 @@ sudo apt-get remove libreoffice-common
 sudo apt-get remove unity-webapps-common 
 sudo apt-get remove thunderbird totem rhythmbox empathy brasero simple-scan gnome-mahjongg aisleriot 
 sudo apt-get remove gnome-mines cheese transmission-common gnome-orca webbrowser-app gnome-sudoku  landscape-client-ui-install  
-
 sudo apt-get remove onboard deja-dup 
+sudo apt install git
+sudo apt install vim
 sudo apt-get install unity-tweak-tool 
+sudo dpkg -i sogou.deb
+sudo dpkg -i teaviewer.deb
+sudo dpkg -i chrome.deb
+sudo apt intall -f
+sudo dpkg -i chrome.deb
+sudo dpkg -i cloudmusic.deb
+sudo apt intall -f
+sudo dpkg -i cloudmusic.deb
+sudo dpkg -i code.deb
+sudo tar -zcvf typora.tar.gz
+
