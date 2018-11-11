194e2eb352785da08234b141791a7cb58ac21853
iron-rao <you@example.com>
2018-11-11 15:09:25 +0800

    2018.11.11

---
+++ beautify.sh
@@ -0,0 +1,34 @@
+sudo apt update
+unzip icons.zip
+
+cd papirus-icon-theme-master
+sudo mv Papirus /usr/share/icons
+cd ..
+unzip themes.zip
+cd vimix-gtk-themes-master
+
+
+
+
+
+sudo mv src /usr/share/themes
+cd ..
+
+
+unzip wine.zip
+cd deepin-wine-ubuntu-master
+sh ./install.sh
+cd ..
+sudo dpkg -i qq.deb
+sudo dpkg -i wechat.deb
+sudo dpkg -i foxmail.deb
+sudo dpkg -i baidupan.deb
+
+
+
+
+
+
+
+
+
