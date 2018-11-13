d071fdefe683e557f2cc0e8ed9a32ce2ff5a65f7
iron-rao <1849473014@qq.com>
2018-11-13 16:41:57 +0800

    2018.11.13

--- beautify.sh
+++ beautify.sh
@@ -1,31 +1,48 @@
-sudo apt update
-unzip icons.zip
 
+sudo apt-get update 
+sudo apt-get upgrade
+sudo add-apt-repository ppa:noobslab/icons  
+sudo apt-get update  
+sudo apt-get install ultra-flat-icons
+apt-get install fcitx
+apt-get install fcitx-googlepinyin
+unzip wine.zip
+cd deepin-wine-ubuntu-master
+sh ./install.sh
+cd ..
+sudo apt-get purge firefox firefox-locale-en unity-scope-firefoxbookmarks
+sudo dpkg -i qq.deb
+sudo dpkg -i wechat.deb
+sudo dpkg -i foxmail.deb
+sudo dpkg -i baidupan.deb
+sudo apt install git
+sudo apt install vim
+sudo apt-get install unity-tweak-tool 
+sudo dpkg -i sogou.deb
+sudo dpkg -i teaviewer.deb
+sudo dpkg -i chrome.deb
+sudo apt intall -f
+sudo dpkg -i chrome.deb
+sudo dpkg -i cloudmusic.deb
+sudo apt intall -f
+sudo dpkg -i cloudmusic.deb
+sudo dpkg -i code.deb
+sudo tar -zcvf typora.tar.gz
+sudo apt-get remove libreoffice-common 
+sudo apt-get remove unity-webapps-common 
+sudo apt-get remove thunderbird totem rhythmbox empathy brasero simple-scan gnome-mahjongg aisleriot 
+sudo apt-get remove gnome-mines cheese transmission-common gnome-orca webbrowser-app gnome-sudoku  landscape-client-ui-install  
+sudo apt-get remove onboard deja-dup 
+unzip icons.zip
 cd papirus-icon-theme-master
 sudo mv Papirus /usr/share/icons
 cd ..
 unzip themes.zip
 cd vimix-gtk-themes-master
-
-
-
-
-
 sudo mv src /usr/share/themes
 cd ..
-
-
-unzip wine.zip
-cd deepin-wine-ubuntu-master
-sh ./install.sh
-cd ..
-sudo dpkg -i qq.deb
-sudo dpkg -i wechat.deb
-sudo dpkg -i foxmail.deb
-sudo dpkg -i baidupan.deb
-
-
-
+sudo unzip Flatabulous-master.zip
+sudo mv Flatabulous-master /usr/share/themes
 
 
 
--- git.sh
+++
@@ -1,3 +0,0 @@
-git add -A
-git commit -m"2018.11.10  not completed."
-git push
